abf028884541cbb249ed8ce1896c3fd4633d64d3
Monica Gerard <mlgerardvla@gmail.com>
2020-03-16 18:14:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -96,7 +96,7 @@ def all_holidays_with_bbq(holiday_hash)
 
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
-      if supplies.includes?("BBQ")
+      if supplies.include?("BBQ")
         bbq_holiday_keys << holiday
         binding.pry
       end
